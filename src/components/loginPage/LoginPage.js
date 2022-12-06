6e380860091afdbf78bc089e0138f0e5a0902d5e
RaeesaParker <raeesaparker101@gmail.com>
2022-12-06 12:32:18 +0000

    Remove empty spaces

--- src/components/loginPage/LoginPage.js
+++ src/components/loginPage/LoginPage.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './LoginPageStyles.css';
+import React from "react";
+import "./LoginPageStyles.css";
 
 // Login Page => Form to sign in => Form to register
 
-function LoginPage(){
-
-  return(
+function LoginPage() {
+  return (
     <div>
       <h1>Welcome to the Login Page</h1>
     </div>
-  )
+  );
 }
 
 export default LoginPage;
--- src/pages/userPage/UserPage.js
+++ src/pages/userPage/UserPage.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './UserPageStyles.css';
+import React from "react";
+import "./UserPageStyles.css";
 
 // User Page => Navigation bar on Left => User Posts Section in Center => User Editing Panel on Right
 
-function UserPage(){
-
-  return(
+function UserPage() {
+  return (
     <div>
       <h1>Welcome to the User Page</h1>
     </div>
-  )
+  );
 }
 
 export default UserPage;
