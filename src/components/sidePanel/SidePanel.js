3613bb4cfc32aed5259068dccd6172321f55a341
mishal008 <mistye999@gmail.com>
2022-12-07 14:11:47 +0000

    changes

--- src/components/sidePanel/SidePanel.js
+++ src/components/sidePanel/SidePanel.js
@@ -1,12 +1,118 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./SidePanelStyles.css";
-
-// Side Panel => Username Bar => Users List => Weather Bar
+// Import profile pictures
+import userProfile from "../../imgs/profiles/profile1.jpg";
 
 function SidePanel() {
+  // State to store the weather
+  const [weather, setWeather] = useState({});
+  const [weatherImage, setWeatherImage] =
+    useState("sunny");
+
+  // Function to set the weather
+  function setWeatherFunc() {
+    setWeather({
+      minTemp: 5,
+      maxTemp: 15,
+      weatherCode: 91,
+    });
+  }
+
+  // Set the weather when the component loads
+  useEffect(() => {
+    setWeatherFunc();
+    setWeatherImageFunc();
+  }, []);
+
+  // Set the weather image when the weather data is loaded
+  useEffect(() => {
+    setWeatherImageFunc();
+  }, [weather]);
+
+  // Function to set the weather image
+  function setWeatherImageFunc() {
+    if (weather.weatherCode == 0) {
+      setWeatherImage("sunny");
+    } else if (
+      weather.weatherCode > 0 &&
+      weather.weatherCode < 50
+    ) {
+      setWeatherImage("cloudy");
+    } else if (
+      weather.weatherCode > 50 &&
+      weather.weatherCode < 60
+    ) {
+      setWeatherImage("cloudy_rain");
+    } else if (
+      weather.weatherCode > 60 &&
+      weather.weatherCode < 70
+    ) {
+      setWeatherImage("rain");
+    } else if (
+      weather.weatherCode > 70 &&
+      weather.weatherCode < 80
+    ) {
+      setWeatherImage("snow_cloud");
+    } else if (
+      weather.weatherCode > 80 &&
+      weather.weatherCode < 90
+    ) {
+      setWeatherImage("snow");
+    } else {
+      setWeatherImage("thunder");
+    }
+  }
+
+  // Function to set all the users
+  const allUsers = [
+    "profile2",
+    "profile3",
+    "profile4",
+    "profile5",
+  ];
+
+  // Render all the user images
+  const userImages = allUsers.map((user) => {
+    return (
+      <img
+        key={user}
+        src={require(`../../imgs/profiles/${user}.jpg`)}
+        alt="Logo"
+        className="fig-user-all"
+      />
+    );
+  });
+
   return (
-    <div>
-      <h1>Welcome to the Side Panel</h1>
+    <div id="subsection-mainpage-panel">
+      <div id="subsection-panel-user">
+        <div id="subsection-panel-user-div">
+          <h4>Username</h4>
+        </div>
+        <div id="subsection-panel-image-div">
+          <img
+            src={userProfile}
+            alt="Logo"
+            id="fig-user"
+          />
+        </div>
+      </div>
+      <div className="subsection-panel">
+        <h3>Preston</h3>
+        <p>All your connected neighbours...</p>
+        <div>{userImages}</div>
+      </div>
+      <div className="subsection-panel">
+        <h3>Weather</h3>
+        <p>Min Temp: {weather.minTemp}&deg;C</p>
+        <p>Max Temp: {weather.maxTemp}&deg;C </p>
+        <img
+          src={require(`../../imgs/weather/${weatherImage}.png`)}
+          alt="Logo"
+          id="fig-weather"
+        />
+      </div>
     </div>
   );
 }
--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./MainPageStyles.css";
+import SidePanel from "../../components/sidePanel/SidePanel";
+
+// Import Components
+import NavigationBar from "../../components/navigationBar/NavigationBar";
+import MainBody from "../../components/MainBody/MainBody";
 
 // Main Page => Navigation bar on Left => Posts Section in Center => User Panel on Right
 
 function MainPage() {
   return (
-    <div>
-      <h1>Welcome to the Main Page</h1>
+    <div id="section-mainpage-div">
+      <div id="subsection-mainpage-navbar">
+        <NavigationBar />
+      </div>
+      <div id="subsection-mainpage-posts">
+        <MainBody />
+      </div>
+      <div id="subsection-mainpage-sidepanel">
+        <SidePanel />
+      </div>
     </div>
   );
 }
