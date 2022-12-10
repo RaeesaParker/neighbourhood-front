0032c38c807776af916f65fa3ba9bc2c98f3fb29
Mayranne <mayranne-@hotmail.com>
2022-12-10 13:47:35 +0000

    add extra users to the imgs profiles pics

--- src/components/sidePanel/SidePanel.js
+++ src/components/sidePanel/SidePanel.js
@@ -66,10 +66,21 @@ function SidePanel(props) {
 
   // Function to set all the users
   const allUsers = [
+    "profile1",
     "profile2",
     "profile3",
     "profile4",
     "profile5",
+    "profile6",
+    "profile7",
+    "profile8",
+    "profile9",
+    "profile10",
+    "profile11",
+    "profile12",
+    "profile13",
+    "profile14",
+    "profile15",
   ];
 
   // Render all the user images
