259f2484746252a17869496e28a2f7f9f00ed991
RaeesaParker <raeesaparker101@gmail.com>
2022-12-10 17:14:48 +0000

    Add hospital page

--- src/App.js
+++ src/App.js
@@ -109,7 +109,9 @@ function App() {
           path="/hospitals"
           element={
             <HospitalPage
+              setUserDetails={onSetUserDetails}
               userDetails={userDetails}
+              isLoggedIn={isLoggedIn}
               setIsLoggedIn={setIsLoggedIn}
             />
           }
--- src/components/allHospital/HospitalBody/HospitalBody.js
+++ src/components/allHospital/HospitalBody/HospitalBody.js
@@ -7,18 +7,26 @@ import SpanHospital from "../../allHospital/SpanHospital/SpanHospital";
 import InfoCards from "../../allShared/InfoCards/InfoCards";
 
 const HospitalBody = () => {
+  const hospitalsArray = [
+    {
+      Name: "Smith Medical",
+      Type: "Pharmacy",
+      Street: "2 Main Drive",
+      Postcode: "PR7 8GH",
+      Telephone: "01772 640128",
+      Website: "https://google.com",
+    },
+  ];
   return (
     <div className="mainbody-box">
       <SearchBox />
       <SpanHospital />
       <div className="hospitalbody-posts">
-        <InfoCards />
-        <InfoCards />
-        <InfoCards />
-        <InfoCards />
-        <InfoCards />
-        <InfoCards />
-        <InfoCards />
+        {hospitalsArray.map((school, i) => {
+          return (
+            <InfoCards key={i} school={school} />
+          );
+        })}
       </div>
     </div>
   );
