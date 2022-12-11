b15f249593e4326150791b6ddf6189a85c2f6118
RaeesaParker <raeesaparker101@gmail.com>
2022-12-11 21:14:28 +0000

    Add style changes to saved postcard

--- src/components/allAccountPage/SavedPostsCard/SavedPostsCard.js
+++ src/components/allAccountPage/SavedPostsCard/SavedPostsCard.js
@@ -5,7 +5,19 @@ import "./SavedPostsCard.css";
 
 const SavedPostsCard = (props) => {
   return (
-    <div className="saved-postcard saved-postcard-bg2">
+    <div
+      className={`saved-postcard ${
+        props.post.post_type === 1
+          ? "saved-postcard-bg1"
+          : props.post.post_type === 2
+          ? "saved-postcard-bg2"
+          : props.post.post_type === 3
+          ? "saved-postcard-bg3"
+          : props.post.post_type === 4
+          ? "saved-postcard-bg4"
+          : "postcard-bg"
+      }`}
+    >
       <div className="saved-postcard-head">
         <div className="saved-postcard-user">
           <i className="fa-solid fa-user-large"></i>
